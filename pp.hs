2dfb02291abe42c637c5d096a697c683cf1b1b8e
hagensopher <hagensopher13@gmail.com>
2021-12-07 21:17:07 -0600

    bleh

--- pp.hs
+++ pp.hs
@@ -4,4 +4,19 @@ import Parsing
 
 myParser = do x <- sat isUpper
               y <- sat isLower
-              return (if x==toUpper y then x:[y] else failure)
+              return (if x==toUpper y then x:[y] else [])
+
+
+
+openBr = char '['
+closeBr = char ']'
+openPn = char '('
+closePn = char ')'
+star = char '*'
+myParser2 :: Parser [Char]
+myParser2 = (many1 star >>= \vs -> myParser >>= \x -> return (x + (length vs))) +++
+  (openBr >> myParser >>= \c -> closeBr >>
+    myParser >>= \d -> return (c + d)) +++
+  (openPn >> myParser >>= \c -> closePn >>
+    myParser >>= \d -> return (c + d)) +++
+  (return 0)
